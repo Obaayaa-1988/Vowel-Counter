0ca46ae9c439fab9d9f88d6c46adf3b61021cb22
Obaayaa-1988 <aquaicoe31@gmail.com>
2023-04-12 23:34:16 +0100

    added event listener and function for count vowel logics

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const word = document.querySelector(".input-text");
 const btn = document.querySelector(".btn");
 const result = document.querySelector(".result");
+
+
+btn.addEventListener("click", countVowels);
+
+function countVowels(){
+    
+}
